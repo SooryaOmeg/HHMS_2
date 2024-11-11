8d854440a45a215410b44482b041e982573ef577
SooryaOmeg <sooryasiva17@gmail.com>
2024-11-11 20:20:19 +0530

    Trying to commit

---
+++ README.md
@@ -0,0 +1,11 @@
+---
+title: HHMS
+emoji: 👁
+colorFrom: green
+colorTo: red
+sdk: docker
+pinned: false
+short_description: SE Exp 10 Dev ops
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
--- website/__init__.py
+++ website/__init__.py
@@ -11,10 +11,12 @@ def create_app():
     logging.basicConfig(level=logging.DEBUG)
     app = Flask(__name__, static_folder='static')
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')  # Set your JWT secret key
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = (f'mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}'
-                                             f'@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}')
+    app.config['SQLALCHEMY_DATABASE_URI'] = (f'mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}')
+
+    print("Change1")
+    print("Added New Stuff. Please Detect")
 
     try:
         db.init_app(app)
