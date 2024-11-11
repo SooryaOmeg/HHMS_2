046d71ea128e6ab713d11fb8789aee11fb091e70
SooryaOmeg <sooryasiva17@gmail.com>
2024-11-11 23:40:09 +0530

    Update __init__.py saved sql to sql_uri

--- website/__init__.py
+++ website/__init__.py
@@ -13,7 +13,8 @@ def create_app():
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = (f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}")
+    # app.config['SQLALCHEMY_DATABASE_URI'] = (f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQL_URI')
 
     print("Change1")
     print("Added New Stuff. Please Detect")
