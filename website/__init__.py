40e832f1c2aa6ad24a083c6d4a631c63bcfef3d2
SooryaOmeg <sooryasiva17@gmail.com>
2024-11-11 23:28:54 +0530

    Update __init__.py gorr sql uri on one line and used "" instead of ''

--- website/__init__.py
+++ website/__init__.py
@@ -13,7 +13,7 @@ def create_app():
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = (f'mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}')
+    app.config['SQLALCHEMY_DATABASE_URI'] = (f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}")
 
     print("Change1")
     print("Added New Stuff. Please Detect")
